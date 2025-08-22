1ba25d706361bd00a69dffacc8d60d3bae429247
Jeremy <spamalotsmileface@gmail.com>
2025-08-22 09:06:40 -0700

    Update build.gradle.kts

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,13 +1,27 @@
 plugins {
     `java-library`
     alias(libs.plugins.lombok)
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
+
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
+    // Provided by server
     compileOnly(libs.paper)
-    compileOnly(libs.guice)
+
+    // Needed at runtime → shade into jar
+    implementation(libs.guice)
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.bstats:bstats-bukkit:3.0.2")
+}
+
+// Shadow config to relocate shaded deps
+tasks.shadowJar {
+    relocate("com.google.gson", "de.rexlmanu.fairychat.libs.gson")
+    relocate("org.bstats", "de.rexlmanu.fairychat.libs.bstats")
+    relocate("com.google.inject", "de.rexlmanu.fairychat.libs.guice")
 }
