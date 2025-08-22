906daa02d20b0995acf5687fde2983b30f0ce763
Jeremy <spamalotsmileface@gmail.com>
2025-08-22 09:39:21 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
 tasks {
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
+        options.release.set(21)
     }
     build {
         dependsOn("shadowJar")
