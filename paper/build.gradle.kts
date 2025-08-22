3c8204fe8192996e0862427abd91d952dd4e37ea
Jeremy <spamalotsmileface@gmail.com>
2025-08-22 09:16:53 -0700

    Update build.gradle.kts

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -12,15 +12,8 @@ dependencies {
     // Provided by server
     compileOnly(libs.paper)
 
-    // Needed at runtime → shade into jar
+    // Needed at runtime (shaded by root build)
     implementation(libs.guice)
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.bstats:bstats-bukkit:3.0.2")
-}
-
-// Shadow config to relocate shaded deps
-tasks.shadowJar {
-    relocate("com.google.gson", "de.rexlmanu.fairychat.libs.gson")
-    relocate("org.bstats", "de.rexlmanu.fairychat.libs.bstats")
-    relocate("com.google.inject", "de.rexlmanu.fairychat.libs.guice")
 }
